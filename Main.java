9211a4086d41ff14c75e5c6fe342a1300935b577
danyewest97 <dane20052008@gmail.com>
2025-08-31 20:25:47 -0700

    temp

--- Main.java
+++ Main.java
@@ -1,14 +1,14 @@
 public class Main {
-   // public native float[] helloWorld(int num_vectors);
+    public native void test();
 
     // Runs when the class is loaded (aka immediately after compilation)
-    //static {
-    //    System.loadLibrary("native");
-    //}
+    static {
+        System.loadLibrary("native");
+    }
 
     public static void main(String[] args) {
         System.out.println("Running!");
-
+        new Main().test();
         System.out.println("\nProgram finished!");
     }
 }
