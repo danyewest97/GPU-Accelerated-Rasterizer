336a859411657e03e7ea89afd0379873efe704ff
danyewest97 <dane20052008@gmail.com>
2025-10-05 00:31:10 -0700

    Make dimensions width and height integers instead of doubles, continue fleshing out (possibly finish? but still needs testing) generate_primary_rays() method

--- structs.cpp
+++ structs.cpp
@@ -12,42 +12,42 @@ struct color {
 
 // A template for a material with different parameters that control how the material interacts with light (used in calculating BRDFs)
 struct material {
-    color material_color;
+    color* material_color;
     double absorption, reflection, transmission, diffusion;
 };
 
 // A 3D plane with components a, b, c, d, expressed by equation ax + by + cz + d = 0
 struct plane {
-    vector normal;                      // vector to store the components of the plane's normal as (a, b, c)
+    vector* normal;                      // vector to store the components of the plane's normal as (a, b, c)
     double d;
 };
 
 // A 3D Ray that starts from the 3D point origin and points in the direction given by the direction vector
 struct ray {
-    vector origin;
-    vector direction;
+    vector* origin;
+    vector* direction;
 };
 
 // A 3D triangle defined by the 3 vectors a, b, and c, with the given material and plane
 struct triangle {
-    plane surface_plane;                // the 3D plane that the triangle sits on
-    material surface_material;          // the material that the triangle is "made out of," defining how light rays should interact with the triangle
-    vector a;
-    vector b;
-    vector c;
+    plane* surface_plane;                // the 3D plane that the triangle sits on
+    material* surface_material;          // the material that the triangle is "made out of," defining how light rays should interact with the triangle
+    vector* a;
+    vector* b;
+    vector* c;
 };
 
 // A container to hold the height and width of an image (or anything else with height and width)
 struct dimensions {
-    double width, height;
+    int width, height;
 };
 
 // 3D camera, defines where the camera rays originate and in which direction they radiate, to control where the viewport is looking
 struct camera {
-    vector origin;                      // The 3D point where all camera rays originate from
-    vector rotation;                    // The direction where camera rays radiate from the origin, with components (x_rotation, y_rotation, 
+    vector* origin;                      // The 3D point where all camera rays originate from
+    vector* rotation;                    // The direction where camera rays radiate from the origin, with components (x_rotation, y_rotation, 
                                         // z_rotation)
-    double vfov, hfov;                  // The field-of-view parameters, expressed in radians, that define how far left/right or up/down the camera 
+    double fov_scale;                  // The field-of-view parameters, expressed in radians, that define how far left/right or up/down the camera 
                                         // can see
 };
 
@@ -55,7 +55,7 @@ struct camera {
 // // Constructors for above-defined structs
 // vector constructor
 __device__ vector* new_vector(double x, double y, double z) {
-    vector* v = new vector[1]];
+    vector* v = new vector[1];
     v->x = x;
     v->y = y;
     v->z = z;
@@ -74,7 +74,7 @@ __device__ color* new_color(double r, double g, double b) {
 // material constructor
 __device__ material* new_material(color* material_color, double absorption, double reflection, double transmission, double diffusion) {
     material* m = new material[1];
-    m->material_color = *material_color;
+    m->material_color = material_color;
     m->absorption = absorption;
     m->reflection = reflection;
     m->transmission = transmission;
@@ -85,7 +85,7 @@ __device__ material* new_material(color* material_color, double absorption, doub
 // plane constructor
 __device__ plane* new_plane(vector* normal, double d) {
     plane* p = new plane[1];
-    p->normal = *normal;
+    p->normal = normal;
     p->d = d;
     return p;
 }
@@ -93,24 +93,24 @@ __device__ plane* new_plane(vector* normal, double d) {
 // ray constructor
 __device__ ray* new_ray(vector* origin, vector* direction) {
     ray* r = new ray[1];
-    r->origin = *origin;
-    r->direction = *direction;
+    r->origin = origin;
+    r->direction = direction;
     return r;
 }
 
 // triangle constructor
 __device__ triangle* new_triangle(plane* surface_plane, material* surface_material, vector* a, vector* b, vector* c) {
     triangle* t = new triangle[1];
-    t->surface_plane = *surface_plane;
-    t->surface_material = *surface_material;
-    t->a = *a;
-    t->b = *b;
-    t->c = *c;
+    t->surface_plane = surface_plane;
+    t->surface_material = surface_material;
+    t->a = a;
+    t->b = b;
+    t->c = c;
     return t;
 }
 
 // dimensions constructor
-__device__ dimensions* new_dimensions(double width, double height) {
+__device__ dimensions* new_dimensions(int width, int height) {
     dimensions* d = new dimensions[1];
     d->width = width;
     d->height = height;
@@ -118,12 +118,11 @@ __device__ dimensions* new_dimensions(double width, double height) {
 }
 
 // camera constructor
-__device__ camera* new_camera(vector* origin, vector* rotation, double vfov, double hfov) {
+__device__ camera* new_camera(vector* origin, vector* rotation, double fov_scale) {
     camera* c = new camera[1];
-    c->origin = *origin;
-    c->rotation = *rotation;
-    c->vfov = vfov;
-    c->hfov = hfov;
+    c->origin = origin;
+    c->rotation = rotation;
+    c->fov_scale = fov_scale;
     return c;
 }
 
